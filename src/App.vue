edf46b9ef266c330c3b4c79155f63e7d3483ed85
Aaron Kummer <aaronjkummer@gmail.com>
2022-08-07 21:10:37 -0400

    styling changes for login section

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 
 <template>
   <ion-app>
-    <ion-grid class="app" v-if="!loggedIn">
-      <ion-row>
-        <img class="grocs" src="./assets/groceries.jpg">
+    <ion-grid v-if="!loggedIn">
+      <ion-row class="ion-justify-content-center">
+        <!-- <img class="grocs" src="./assets/groceries.jpg"> -->
+        <ion-col size="12">
+          <h1 style="text-align:center; font-size: 38px;">Grocery Helper</h1>
+          <br><hr>
+        </ion-col>
       </ion-row>
       <ion-row>
         <ion-col>
           <ion-item color="secondary">
-            <ion-label>E-mail: </ion-label> <ion-input v-model="email"></ion-input>
+            <ion-label>E-mail: </ion-label>
+            <ion-input v-model="email"></ion-input>
           </ion-item>
         </ion-col>
         <ion-col>
           <ion-item color="secondary">
-            <ion-label>Password: </ion-label> <ion-input v-model="password"></ion-input>
+            <ion-label>Password: </ion-label>
+            <ion-input v-model="password"></ion-input>
           </ion-item>
         </ion-col>
       </ion-row>
-      <ion-row>
-        <ion-button style="margin-left:90%" color="secondary" @click="login">Login</ion-button>
+      <ion-row class="ion-justify-content-end">
+        <ion-button color="secondary" @click="login"
+          >Login</ion-button
+        >
       </ion-row>
     </ion-grid>
-    
+
     <ion-router-outlet v-if="loggedIn" />
   </ion-app>
 </template>
@@ -39,8 +47,8 @@ export default defineComponent({
   data: function () {
     return {
       loggedIn: false,
-      email: '',
-      password: ''
+      email: "",
+      password: "",
     };
   },
   methods: {
@@ -52,19 +60,16 @@ export default defineComponent({
 </script>
 
 <style>
-.app {
-  background-color: white;
-}
 .grocs {
   width: 20% !important;
   margin-left: 40%;
 }
-@media (prefers-color-scheme: light) {
+@media (prefers-color-scheme: dark) {
   :root {
     /* dark mode variables go here */
   }
 }
-ion-input{
- /* background-color: white !important; */
+ion-input {
+  /* background-color: white !important; */
 }
 </style>
